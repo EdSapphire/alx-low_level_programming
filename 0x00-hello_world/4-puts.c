b4b1a30f18d4a0c25a952b9f5dfea31d1879ab68
Edward Ebenezer <alabish@gmail.com>
2023-03-18 01:13:02 -0700

    First C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/*
+ * File: 4-puts.c
+ *
+ * main: Prints "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
