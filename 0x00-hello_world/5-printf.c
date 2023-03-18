f80d606ee8a73e158f7a2fe259a2bb669e237360
Edward Ebenezer <alabish@gmail.com>
2023-03-18 06:57:32 -0700

    Print with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: Prints with the printf function.
+ * main - Prints "with proper grammar,
+ * but the outcome is a piece of art,
  *
  * Return: Always 0.
  */
 int main(void)
-{	printf("\"with proper grammer, but the outcome is a piece of art,");
+{	printf("\"with proper grammar, but the outcome is a piece of art,");
 
-	return 0;
+	return (0);
 }
