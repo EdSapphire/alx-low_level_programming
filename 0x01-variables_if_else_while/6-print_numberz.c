1f99375c6a7c78631a3dab6ae97f458780b3f0d9
Edward Ebenezer <alabish@gmail.com>
2023-03-20 14:18:10 -0700

    Print single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Declare a variable and assign interger value
+ * increment the value
+ * print the variable
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
