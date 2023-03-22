8805348bacf9c1c5b585f89416e515711951f724
Edward Ebenezer <alabish@gmail.com>
2023-03-22 04:32:29 -0700

    Print number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: number to be checked
+ *
+ * Return: 1 if positive, -1 if negative 
+ * and 0 if zero
+ */
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 48)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		_putchar('0');
+	return (0);
+}
