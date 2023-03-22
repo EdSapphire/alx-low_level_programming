2050d7b69c493a0eda26d44fb2077a34c3432221
Edward Ebenezer <alabish@gmail.com>
2023-03-22 04:43:19 -0700

    Print number sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,22 +4,22 @@
  * print_sign - Prints the sign of a number
  * @n: number to be checked
  *
- * Return: 1 if positive, -1 if negative 
+ * Return: 1 if positive, -1 if negative
  * and 0 if zero
  */
 int print_sign(int n)
 {
 	if (n > 48)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 48)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
 	else
-		_putchar('0');
+		_putchar(48);
 	return (0);
 }
