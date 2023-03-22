9f83e54518be31874aadf5f7f9544d3a3767bd0f
Edward Ebenezer <alabish@gmail.com>
2023-03-22 08:37:26 -0700

    Absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute number of an integer
+ * @a: number to be computed
+ *
+ * Return: Absolute value of number or zero
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		int abs_val;
+
+		abs_val = a * -1;
+		return (abs_val);
+	}
+	return (a);
+}
