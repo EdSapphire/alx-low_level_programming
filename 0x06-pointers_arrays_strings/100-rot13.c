2ef3a2be135d46a12e95297886ce2ae44c0c6c68
Edward Ebenezer <alabish@gmail.com>
2023-03-30 16:56:13 -0700

    Rot13 encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - A function that encodes a string using rot13
+ * @src: String to encode
+ *
+ * Return: Encoded string
+ */
+char *rot13(char *str)
+{
+	int alpha = 0, letter;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[alpha])
+	{
+		for (letter = 0; letter < 52; letter++)
+		{
+			if (str[alpha] == alphabet[letter])
+			{
+				str[alpha] = rot13key[letter];
+				break;
+			}
+		}
+
+		alpha++;
+	}
+
+	return (str);
+}
