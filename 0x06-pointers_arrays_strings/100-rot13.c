50a4b7c8060c79d1da5e7cb020eff362ac373863
Edward Ebenezer <alabish@gmail.com>
2023-03-30 17:09:34 -0700

    Rot13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,8 +9,7 @@
  */
 char *rot13(char *str)
 {
-	int alpha = 0, letter;
-	
+	int alpha = 0, letter;	
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 			     'G', 'H', 'I', 'J', 'K', 'L',
 			     'M', 'N', 'O', 'P', 'Q', 'R',
@@ -20,7 +19,6 @@ char *rot13(char *str)
 			     'k', 'l', 'm', 'n', 'o', 'p',
 			     'q', 'r', 's', 't', 'u', 'v',
 			     'w', 'x', 'y', 'z'};
-	
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 			     'T', 'U', 'V', 'W', 'X', 'Y',
 			     'Z', 'A', 'B', 'C', 'D', 'E',
@@ -30,7 +28,6 @@ char *rot13(char *str)
 			     'x', 'y', 'z', 'a', 'b', 'c',
 			     'd', 'e', 'f', 'g', 'h', 'i',
 			     'j', 'k', 'l', 'm'};
-
 	while (str[alpha])
 	{
 		for (letter = 0; letter < 52; letter++)
