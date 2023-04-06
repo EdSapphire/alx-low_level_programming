2f4744598d093d5a4f0d3ba2073118d2ede00509
Edward Ebenezer <alabish@gmail.com>
2023-04-05 21:31:29 -0700

    Return natural square root of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,18 +3,33 @@
 #include <math.h>
 
 /**
+ * _newRec - A function that returns the square root of two numbers
+ * @n: Main number
+ * @m: Number to check for square root
+ *
+ * Return: Square root.
+ */
+int _newRec(int n, int m)
+{
+	if (m * m > n)
+		return (-1);
+	else if (m * m == n)
+		return (m);
+	else
+		return (_newRec(n, m + 1));
+	return (1);
+}
+
+/**
  * _sqrt_recursion - A function that returns the natural square root of a number.
  * @n: Number to return its square root
  *
  * Return: negative for non-natural number
  * positive for natual numbers
  */
-int _sqrt_recursion(int n, int new)
+int _sqrt_recursion(int n)
 {
-	int cur_new = (new + n / new) / 2;
-
-	if (fabs(cur_new * cur_new - n) < 0)
-		return (cur_new);
-	else
-		return (_sqrt_recursion(n, cur_new));
+	if (n == 0)
+		return (0);
+	return (_newRec(n, 1));
 }
