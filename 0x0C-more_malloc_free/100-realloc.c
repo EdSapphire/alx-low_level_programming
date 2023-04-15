bffbd8a2d7250a35422412b6e897d8a8617dda02
Edward Ebenezer <alabish@gmail.com>
2023-04-14 19:08:35 -0700

    Reallocate a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -41,7 +41,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	
 	for (i = 0; i < old_size && i < new_size; i++)
 		s[i] = ((char *) ptr)[i];
-
+	
 	free(ptr);
 	return (s);
 }
