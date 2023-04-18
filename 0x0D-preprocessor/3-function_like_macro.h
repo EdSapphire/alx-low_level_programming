36a7e142a39265d3bbf58c073822861233a1798f
Edward Ebenezer <alabish@gmail.com>
2023-04-18 00:37:28 -0700

    Computes the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,10 +2,10 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /**
- * define - A funtion-like macro that computes
+ * define - A function-like macro that computes
  * the absolute value of a number
  *
  */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
-#define ABS(x) ((x) < (0) ? (-x) : (x))
 #endif
