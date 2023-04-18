dead5982a49c80561e715a1bd96f47eacd274eed
Edward Ebenezer <alabish@gmail.com>
2023-04-17 20:15:21 -0700

    Header file that defines a macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,11 +2,10 @@
 #define OBJECT_LIKE_MACRO_H
 
 /**
- * Header file that defines a macro named SIZE as
- * an abbreviation for the token 1024.
+ * @define: Header file that defines a macro named SIZE as
+ * an abbreviation for the token 1024
  *
  */
-
 #define SIZE 1024
 
 #endif
