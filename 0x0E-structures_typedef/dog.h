98334535eed9404053608ccc10d6672229838cde
Edward Ebenezer <alabish@gmail.com>
2023-04-18 18:15:31 -0700

    dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,11 @@
+#include <stdio.h>
+
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - Defines a new dog
- * @name: Dog name
- * @age: Dog age
- * @owner: Dog owner
- *
- * Description: A struct that assign the name, age and owner of a new dog
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
