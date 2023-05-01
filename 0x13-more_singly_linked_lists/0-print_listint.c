11e12cd12017607b881c971cfb95d8dbacc51888
Edward Ebenezer <alabish@gmail.com>
2023-05-01 14:44:37 -0700

    Print elements of a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t newlist = 0;
-	
+	int size = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		newlist++;
+		size++;
 		h = h->next;
 	}
-	return (newlist);
+	return (size);
 }
