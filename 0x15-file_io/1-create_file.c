9e35bae6081e3f2777565ecb299398f30c4938a4
Edward Ebenezer <alabishg@gmail.com>
2023-05-12 05:26:44 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int wr;
-	int len = strlen(text_content);
+	int len;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,6 +21,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
+		len = strlen(text_content);
 		wr = write(fd, text_content, len);
 		close(fd);
 		if (wr == -1)
