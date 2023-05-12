7e89b450c4d7e4b90f4e53a13c655451d02c6c0b
Edward Ebenezer <alabishg@gmail.com>
2023-05-12 06:09:42 -0700

    Append a Text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,17 +10,13 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-        int wr;
-        int len;
-
-        if (filename == NULL)
-                return (-1);
+	int fd, wr, len;
 	
+	if (filename == NULL)
+                return (-1);	
 	fd = open(filename, O_WRONLY | O_APPEND);
         if (fd == -1)
-                return (-1);
-	
+                return (-1);	
 	if (text_content != NULL)
         {
                 len = strlen(text_content);
@@ -28,7 +24,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (wr == -1)
                         return (-1);
         }
-
         close(fd);
         return (1);
 }
